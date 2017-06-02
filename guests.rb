2a8e9f339e2a934e15ecab9b3a3e8bb3f52c579b
Alan White <alanwhitey@gmail.com>
2017-06-02 15:47:08 +0100

    these files should have been in last commmit

---
+++ guests.rb
@@ -0,0 +1,15 @@
+class Guest
+  attr_accessor :name, :wallet
+  @name = name
+
+  def initialize(name)
+    @name = name
+    @wallet = []
+  end
+
+
+
+
+
+
+end
---
+++ rooms.rb
@@ -0,0 +1,38 @@
+require("pry")
+class Room
+  attr_accessor :name, :singers, :songs
+
+  def initialize(name)
+    @name = name
+    @singers = []
+    @songs = []
+  end
+
+  def singers_in_room()
+    return @singers.count()
+  end
+
+  def add_guest_to_singers(guest)
+    @singers << guest
+  end
+
+  def remove_guest_from_singers(guest)
+    @singers.delete(guest)
+  end
+
+  def records_in_room()
+    return @songs.count()
+  end
+
+  def add_record_to_songs(record)
+    @songs << record
+  end
+
+  def remove_record_from_songs(record)
+    @songs.delete(record)
+  end
+
+
+
+
+end
